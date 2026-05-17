05a524e905fbad6ed3eaf30b1af1b9b7e1d714a4
SAYAN GHOSH <sayan.23bce11674@vitbhopal.ac.in>
2026-05-17 19:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 38.7 MB (44.80%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        if (start < 0 || start >= arr.size() || arr[start] < 0) return false;
+        if (arr[start] == 0) return true;
+        int step = arr[start];
+        arr[start] = -1; // mark visited
+        return canReach(arr, start + step) || canReach(arr, start - step);
+    }
+};
