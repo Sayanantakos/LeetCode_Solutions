bdaa10c3ecf848c18f2502b4a56d528667c70c7d
SAYAN GHOSH <sayan.23bce11674@vitbhopal.ac.in>
2026-05-21 23:25:44 +0530

    Time: 65 ms (59.66%) | Memory: 80.6 MB (73.48%) - LeetSync

---
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr1) {
+            while (num > 0) {
+                set.add(num);
+                num /= 10;
+            }
+        }
+        int maxLen = 0;
+        for (int num : arr2) {
+            while (num > 0) {
+                if (set.contains(num)) {
+                    maxLen = Math.max(maxLen, String.valueOf(num).length());
+                    break;
+                }
+                num /= 10;
+            }
+        }
+        return maxLen;
+    }
+}
