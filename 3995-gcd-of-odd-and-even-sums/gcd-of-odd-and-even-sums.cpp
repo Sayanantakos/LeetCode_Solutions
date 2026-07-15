6e6e313cdd7daea37375c5cf4a6952b216f8f0cc
SAYAN GHOSH <sayan.23bce11674@vitbhopal.ac.in>
2026-07-15 11:10:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        long long sumOdd = (long long)n * n;
+        long long sumEven = (long long)n * (n + 1);
+        return gcd(sumOdd, sumEven);
+    }
+};
