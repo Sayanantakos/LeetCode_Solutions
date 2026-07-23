3274681d63d5f72cbed9ffa0d26fb0243f179c7b
SAYAN GHOSH <sayan.23bce11674@vitbhopal.ac.in>
2026-07-23 22:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 198.7 MB (85.00%) - LeetSync

---
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) return n;
+        
+        int maxVal = 1;
+        while (maxVal <= n) maxVal <<= 1;
+        return maxVal;
+    }
+};
