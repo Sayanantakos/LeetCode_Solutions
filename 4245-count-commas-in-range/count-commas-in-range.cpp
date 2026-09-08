6c56bbfae4a3251a7a511355bf7abde20b6449ee
SAYAN GHOSH <sayan.23bce11674@vitbhopal.ac.in>
2026-09-09 02:47:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.72%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int totalCommas = 0;
+        long long p = 1000;
+        
+        while (n >= p) {
+            totalCommas += (n - p + 1);
+            p *= 1000;
+        }
+        
+        return totalCommas;
+    }
+};
